87ce9a6fab3730b99db2ba809411939674f132ae
Mahad <mahad.banai@comeon.com>
2026-06-28 17:52:34 +0200

    feat(cache): add redis infrastructure configuration

--- src/main/java/io/qwenbridge/QwenBridgeApplication.java
+++ src/main/java/io/qwenbridge/QwenBridgeApplication.java
@@ -1,5 +1,8 @@
 package io.qwenbridge;
 
+import io.qwenbridge.analysis.cache.config.AIAnalysisCacheProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
