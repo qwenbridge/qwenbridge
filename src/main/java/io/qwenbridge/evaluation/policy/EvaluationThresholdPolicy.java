5e4c01b31758c3bbfb73dad53a6806014ce4e13e
Mahad <mahad.banai@comeon.com>
2026-07-04 00:02:48 +0200

    feat(evaluation): add quality threshold policy

---
+++ src/main/java/io/qwenbridge/evaluation/policy/EvaluationThresholdPolicy.java
@@ -0,0 +1,9 @@
+package io.qwenbridge.evaluation.policy;
+
+import io.qwenbridge.evaluation.model.EvaluationGateResult;
+import io.qwenbridge.evaluation.model.EvaluationResult;
+
+public interface EvaluationThresholdPolicy {
+
+    EvaluationGateResult evaluate(EvaluationResult result);
+}
