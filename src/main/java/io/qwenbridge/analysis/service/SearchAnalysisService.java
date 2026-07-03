7dcd96c7ed0eb0b2e6f33910df4de27095e3559d
Mahad <mahad.banai@comeon.com>
2026-07-03 22:30:21 +0200

    refactor(streaming): make search analysis request-aware

--- src/main/java/io/qwenbridge/analysis/service/SearchAnalysisService.java
+++ src/main/java/io/qwenbridge/analysis/service/SearchAnalysisService.java
@@ -3,5 +3,10 @@ package io.qwenbridge.analysis.service;
 import io.qwenbridge.analysis.model.SearchAnalysis;
 
 public interface SearchAnalysisService {
+
     SearchAnalysis analyze(String query);
+
+    default SearchAnalysis analyze(String query, String requestId) {
+        return analyze(query);
+    }
 }
